400b6394b2e7296c46712f9b7e07a29cd71c8cb6
Raul <plumade.lince2@gmail.com>
2023-05-19 13:54:00 +0200

    React inicial xd

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import reportWebVitals from "./reportWebVitals";
+
+import ErrorPage from "./routes/ErrorPage";
+import Root from "./routes/Root";
+
+import Homepage from "./routes/Homepage";
+import Login from "./routes/Login";
+import Signup from "./routes/Signup";
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />
+      },
+      {
+        path: "/login",
+        element: <Login />
+      },
+      {
+        path: "/signup",
+        element: <Signup />
+      },
+    ]
+  },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
+reportWebVitals();
---
+++ src/routes/Root.js
@@ -0,0 +1,22 @@
+import { Link, Outlet } from "react-router-dom";
+
+export default function Root() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Homepage</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  );
+}
