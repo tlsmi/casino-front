b94ac2a61a7fee41bfd22116941b4bbdb296ac93
Raul <plumade.lince2@gmail.com>
2023-05-25 10:08:36 +0200

    Login-Signup sin css Mensajes de estado, token en localstorage, redirects, validar formulario

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -6,27 +6,74 @@ const Login = () => {
         password: ''
     });
 
+    const [errors, setErrors] = useState({
+        email: '',
+        password: ''
+    });
+
+    const [message, setMessage] = useState('');
+
     const handleSubmit = async (e) => {
         e.preventDefault();
+        if (validateForm()) {
+            try {
+                const response = await fetch('http://localhost:8080/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(formData)
+                });
 
-        try {
-            const response = await fetch('http://localhost:8080/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(formData)
-            });
+                // Handle the response from the server
+                const jsonData = await response.json();
+                setMessage(jsonData.message);
+                // Manejar la respuesta del servidor
+                if (response.ok) {
+                    console.log('Login successful');
+                    localStorage.setItem("token", jsonData.token)
+                    window.location.href = '/'
 
-            // Handle the response from the server
-            if (response.ok) {
-                console.log('Login successful');
-            } else {
-                console.log('Login failed');
+                } else {
+                    console.log('Login failed');
+                }
+            } catch (error) {
+                console.error('Error:', error);
             }
-        } catch (error) {
-            console.error('Error:', error);
         }
+    };
+
+    const validateForm = () => {
+        let valid = true;
+        const newErrors = { ...errors };
+
+        // Validar el email
+        if (!validateEmail(formData.email)) {
+            newErrors.email = 'El email no es válido.';
+            valid = false;
+        } else {
+            newErrors.email = '';
+        }
+
+        // Validar la contraseña
+        if (!validatePassword(formData.password)) {
+            newErrors.password = 'La contraseña debe tener al menos 8 caracteres.';
+            valid = false;
+        } else {
+            newErrors.password = '';
+        }
+
+        setErrors(newErrors);
+        return valid;
+    };
+
+    const validateEmail = (email) => {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    };
+
+    const validatePassword = (password) => {
+        return password.length >= 8;
     };
 
     const handleChange = (e) => {
@@ -43,15 +90,18 @@ const Login = () => {
                 <label>
                     Email:
                     <input type="email" name="email" value={formData.email} onChange={handleChange} />
+                    {errors.email && <span className="error">{errors.email}</span>}
                 </label>
                 <br />
                 <label>
                     Password:
                     <input type="password" name="password" value={formData.password} onChange={handleChange} />
+                    {errors.password && <span className="error">{errors.password}</span>}
                 </label>
                 <br />
                 <button type="submit">Submit</button>
             </form>
+            <p>{message}</p>
         </div>
     );
 };
--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -1,95 +1,178 @@
 import React, { useState } from 'react';
-
 const Signup = () => {
-    const [formData, setFormData] = useState({
-        name: '',
-        surname1: '',
-        surname2: '',
-        email: '',
-        birthDate: '',
-        password: '',
-        passwordRep: '',
-        dni: ''
-    });
+  const [formData, setFormData] = useState({
+    name: '',
+    surname1: '',
+    surname2: '',
+    email: '',
+    birthDate: '',
+    password: '',
+    passwordRep: '',
+    dni: ''
+  });
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const [errors, setErrors] = useState({
+    name: '',
+    surname1: '',
+    surname2: '',
+    email: '',
+    birthDate: '',
+    password: '',
+    passwordRep: '',
+    dni: ''
+  });
 
-        try {
-            const response = await fetch('http://localhost:8080/signup', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(formData)
-            });
+  const [message, setMessage] = useState('');
 
-            // Handle the response from the server
-            if (response.ok) {
-                console.log('Signup successful');
-            } else {
-                console.log('Signup failed');
-            }
-        } catch (error) {
-            console.error('Error:', error);
-        }
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleChange = (e) => {
-        setFormData({
-            ...formData,
-            [e.target.name]: e.target.value
+    // Validar los datos antes de enviar la solicitud
+    if (validateForm()) {
+      try {
+        const response = await fetch('http://localhost:8080/signup', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(formData)
         });
-    };
 
-    return (
-        <div>
-            <h1>Sign Up</h1>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Name:
-                    <input type="text" name="name" value={formData.name} onChange={handleChange} />
-                </label>
-                <br />
-                <label>
-                    First Surname:
-                    <input type="text" name="surname1" value={formData.surname1} onChange={handleChange} />
-                </label>
-                <br />
-                <label>
-                    Second Surname:
-                    <input type="text" name="surname2" value={formData.surname2} onChange={handleChange} />
-                </label>
-                <br />
-                <label>
-                    Email:
-                    <input type="email" name="email" value={formData.email} onChange={handleChange} />
-                </label>
-                <br />
-                <label>
-                    Birth Date:
-                    <input type="date" name="birthDate" value={formData.birthDate} onChange={handleChange} />
-                </label>
-                <br />
-                <label>
-                    Password:
-                    <input type="password" name="password" value={formData.password} onChange={handleChange} />
-                </label>
-                <br />
-                <label>
-                    Repeat Password:
-                    <input type="password" name="passwordRep" value={formData.passwordRep} onChange={handleChange} />
-                </label>
-                <br />
-                <label>
-                    DNI:
-                    <input type="text" name="dni" value={formData.dni} onChange={handleChange} />
-                </label>
-                <br />
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    );
+        const jsonData = await response.json();
+        console.log(jsonData)
+        setMessage(jsonData.message);
+        // Manejar la respuesta del servidor
+        if (response.ok) {
+          console.log('Signup successful');
+          window.location.href = '/login'
+        } else {
+          console.log('Signup failed');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    }
+  };
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const validateForm = () => {
+    let valid = true;
+    const newErrors = { ...errors };
+
+    // Validar el DNI
+    if (!validateDNI(formData.dni)) {
+      newErrors.dni = 'El DNI debe tener 8 números y una letra.';
+      valid = false;
+    } else {
+      newErrors.dni = '';
+    }
+
+    // Validar el email
+    if (!validateEmail(formData.email)) {
+      newErrors.email = 'El email no es válido.';
+      valid = false;
+    } else {
+      newErrors.email = '';
+    }
+
+    // Validar la contraseña
+    if (!validatePassword(formData.password)) {
+      newErrors.password = 'La contraseña debe tener al menos 8 caracteres.';
+      valid = false;
+    } else {
+      newErrors.password = '';
+    }
+
+    // Validar la repetición de la contraseña
+    if (formData.password !== formData.passwordRep) {
+      newErrors.passwordRep = 'Las contraseñas no coinciden.';
+      valid = false;
+    } else {
+      newErrors.passwordRep = '';
+    }
+
+    setErrors(newErrors);
+    return valid;
+  };
+
+  const validateDNI = (dni) => {
+    const dniRegex = /^\d{8}[a-zA-Z]$/;
+    return dniRegex.test(dni);
+  };
+
+  const validateEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  const validatePassword = (password) => {
+    return password.length >= 8;
+  };
+
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" name="name" value={formData.name} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          First Surname:
+          <input type="text" name="surname1" value={formData.surname1} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Second Surname:
+          <input type="text" name="surname2" value={formData.surname2} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input type="email" name="email" value={formData.email} onChange={handleChange} />
+          {errors.email && <span className="error">{errors.email}</span>}
+        </label>
+        <br />
+        <label>
+          Birth Date:
+          <input type="date" name="birthDate" value={formData.birthDate} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" name="password" value={formData.password} onChange={handleChange} />
+          {errors.password && <span className="error">{errors.password}</span>}
+        </label>
+        <br />
+        <label>
+          Repeat Password:
+          <input type="password" name="passwordRep" value={formData.passwordRep} onChange={handleChange} />
+          {errors.passwordRep && <span className="error">{errors.passwordRep}</span>}
+        </label>
+        <br />
+        <label>
+          DNI:
+          <input type="text" name="dni" value={formData.dni} onChange={handleChange} />
+          {errors.dni && <span className="error">{errors.dni}</span>}
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+      <p>{message}</p>
+    </div>
+
+  );
 };
 
 export default Signup;
+
+
+
+
