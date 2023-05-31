cf0bd35ec326fb1186c1e575307511372ad10a9c
Raul <plumade.lince2@gmail.com>
2023-05-31 16:00:34 +0200

    Profile y Password Validaciones y mensajes de error terminados

--- src/App.js
+++ src/App.js
@@ -10,7 +10,8 @@ import Root from "./routes/Root";
 import Homepage from "./routes/Homepage";
 import Login from "./routes/Login";
 import Signup from "./routes/Signup";
-
+import Profile from './routes/Profile';
+import Password from './routes/Password';
 
 
 const router = createBrowserRouter([
@@ -30,6 +31,14 @@ const router = createBrowserRouter([
       {
         path: "/signup",
         element: <Signup />
+      },
+      {
+        path: "/profile",
+        element: <Profile />
+      },
+      {
+        path: "/profile/password",
+        element: <Password />
       },
     ]
   },
